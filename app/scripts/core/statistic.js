ebb81746b8998ba03b9c230f3a0e5a2ef0bc2189
proll <gleb.proll@gmail.com>
2014-10-20 16:35:43 +0400

    старая метрика

--- app/scripts/core/statistic.js
+++ app/scripts/core/statistic.js
@@ -22,49 +22,49 @@ jhst.Statistic = Backbone.Model.extend({
 			window._gaq.push(['_trackPageview', str]);
 		}
 		
-		if(window.yaCounter26609061) {
-			window.yaCounter26609061.hit(jhst.root + str);
+		if(window.yaCounter26254773) {
+			window.yaCounter26254773.hit(jhst.root + str);
 		}
 	},
 
 	trackLandingDetails: function() {
 		if(!this.active) return false;
 		window._gaq.push(['_trackEvent', 'Landing', 'show_details']);
-		window.yaCounter26609061.reachGoal('Landing_show_details');
+		window.yaCounter26254773.reachGoal('Landing_show_details');
 	},
 
 	trackLandingBtn1: function() {
 		if(!this.active) return false;
 		window._gaq.push(['_trackEvent', 'Landing', 'click_btn_1']);
-		window.yaCounter26609061.reachGoal('Landing_click_btn_1');
+		window.yaCounter26254773.reachGoal('Landing_click_btn_1');
 	},
 
 	trackLandingBtn2: function() {
 		if(!this.active) return false;
 		window._gaq.push(['_trackEvent', 'Landing', 'click_btn_2']);
-		window.yaCounter26609061.reachGoal('Landing_click_btn_2');
+		window.yaCounter26254773.reachGoal('Landing_click_btn_2');
 	},
 
 	trackLandingBtn3: function() {
 		if(!this.active) return false;
 		window._gaq.push(['_trackEvent', 'Landing', 'click_btn_3']);
-		window.yaCounter26609061.reachGoal('Landing_click_btn_3');
+		window.yaCounter26254773.reachGoal('Landing_click_btn_3');
 	},
 
 	trackLandingBtnPup: function(metric) {
 		if(!this.active) return false;
 		if(metric === 'feedback') {
 			window._gaq.push(['_trackEvent', 'Landing', 'click_btn_pup']);
-			window.yaCounter26609061.reachGoal('Landing_click_btn_pup');
+			window.yaCounter26254773.reachGoal('Landing_click_btn_pup');
 		} else {
 			window._gaq.push(['_trackEvent', 'Landing', 'click_btn_pup_reg']);
-			window.yaCounter26609061.reachGoal('Landing_click_btn_pup_reg');
+			window.yaCounter26254773.reachGoal('Landing_click_btn_pup_reg');
 		}
 	},
 
 	trackLandingSubmitError: function() {
 		if(!this.active) return false;
 		window._gaq.push(['_trackEvent', 'Landing', 'submit_error']);
-		window.yaCounter26609061.reachGoal('Landing_submit_error');
+		window.yaCounter26254773.reachGoal('Landing_submit_error');
 	},
 });
